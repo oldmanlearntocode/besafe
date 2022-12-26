992d5b22250db6297bbde88a54f35e51b6a4a309
oldmanlearntocode <marioeybers@gmail.com>
2022-12-26 10:02:49 +0200

    26122022

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 var xValues = ["Gaut", "KZN", "WC", "Limp", "NW", "FS", "EC", "NC","MP",];
-var yValues = [39, 15, 21, 3, 8, 2, 4, 1,1,];
+var yValues = [38, 15, 21, 3, 8, 2, 5, 2,1,];
 var barColors = ["red", "green","blue","orange","brown","red", "green","blue","orange","red"];
 
 new Chart("myChart", {
@@ -29,7 +29,7 @@ new Chart("myChart2", {
   data: {
     labels: xValues,
     datasets: [{ 
-      data: [0,1,0,1,0,0,1,6,4,1,4,7,11,3,2,3,3,7,4,1,3,24,8],
+      data: [0,1,0,1,0,0,1,6,4,1,4,7,11,3,2,3,3,7,4,1,3,24,9],
       borderColor: "orange",
       fill: false
     }]
@@ -41,7 +41,7 @@ new Chart("myChart2", {
 
 
 var xValues = ["Ages 10-19","Ages 20-29", "Ages 30-39", "Ages 40+", "Unknown",];
-var yValues = [5, 48, 28, 5, 8,];
+var yValues = [4, 49, 29, 5, 8,];
 var barColors = [
   "#b91d47",
   "#00aba9",
